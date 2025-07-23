b5572223feb6bcceae378184ca461a99866dfbd0
Anamika-SQ <anamika.mukherjee@leads-bd.com>
2025-07-23 17:21:23 +0600

    Initial commit

---
+++ cypress/e2e/CenterPoint/Candidate_portal.cy.js
@@ -0,0 +1,20 @@
+
+import CandidateInfo from '../../ERP/CandidateInfoPage'; // ES Module import
+import CandidateSpouse from '../../ERP/CandidateSpousePage'; // ES Module import
+import 'cypress-file-upload';
+
+describe('PRMS', () => {
+    it('Step 1: Candidate Info', function () {
+
+        const candidateInfo = new CandidateInfo();
+        const candidateSpouse = new CandidateSpouse();
+
+
+
+        candidateInfo.CandidateInfo();
+        candidateSpouse.CandidateSpouse();
+    });
+
+
+
+})
